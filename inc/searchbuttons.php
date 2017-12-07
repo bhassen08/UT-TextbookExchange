445d9ad96e2bb9f415ee769a8745459ecc1f6579
C <C@C-i7>
2017-12-07 18:01:45 -0500

    search buttons dont inlcude current user

--- inc/searchbuttons.php
+++ inc/searchbuttons.php
@@ -2,21 +2,31 @@
 $trade=0;
 $rent=0;
 $sell=0;
-if($results){
-	$isbn13=$results[0]['volumeInfo']['industryIdentifiers'][0]['identifier'];
-	$q = "SELECT * FROM `keeper` WHERE `isbn13`='$isbn13' && `available`='1';";
+
+
+
+if(isset($_SESSION['user'])){ //user is logged in
+	$userName = $db->real_escape_string($_SESSION['user']);
+	// GET USER ID
+	$getUserIdQuery = "SELECT users.id FROM users WHERE username = '$userName'";
+	$userIdResult = $db->query($getUserIdQuery) or die("BAD SQL: $getUserIdQuery");
+	$userId = $userIdResult->fetch_row();
+	$userId=$userId['0'];
+
+	$isbn=$item['volumeInfo']['industryIdentifiers'][0]['identifier'];
+	//echo "<br />isbn:" . $isbn . "<br />user:" . $userId . "<br />";
+	$q = "SELECT * FROM `keeper` WHERE `isbn13`='$isbn' && `available`='1' && `fortrade`='1' && `userid`<>'$userId';";
 	$tradeq = $db->query($q) or die("BAD SQL: ");
 	$trade=$tradeq->num_rows;
-}else{ //item foreach statement
-	$isbn13=$item['volumeInfo']['industryIdentifiers'][0]['identifier'];
-	$q = "SELECT * FROM `keeper` WHERE `isbn13`='$isbn13' && `available`='1';";
-	$tradeq = $db->query($q) or die("BAD SQL: ");
-	$trade=$tradeq->num_rows;
+	
+	echo "<a href=\"./send.php?i=" . $isbn . "\"><button type=\"button\" class=\"btn btn-info\">Trade (" . $trade . ")</button></a> ";
+	echo "<button type=\"button\" class=\"btn btn-info\">Rent (" . $rent . ")</button> ";
+	echo "<button type=\"button\" class=\"btn btn-info\">Purchase (" . $sell . ")</button><br /><br /><br /><br />";
+}else{
+	echo "<button type=\"button\" class=\"btn btn-default\">Login to rent/sell/trade!</button>";
 }
 
 
-echo "<a href=\"./send.php?i=" . $isbn13 . "\"><button type=\"button\" class=\"btn btn-info\">Trade (" . $trade . ")</button></a> ";
-echo "<button type=\"button\" class=\"btn btn-info\">Rent (" . $rent . ")</button> ";
-echo "<button type=\"button\" class=\"btn btn-info\">Purchase (" . $sell . ")</button><br /><br /><br /><br />";
+
 
 ?>
