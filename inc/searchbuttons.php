5947945492ace30445364f4187c57b6b89333222
C <C@C-i7>
2017-12-06 22:46:51 -0500

    added features for message system and fixed slashes for includes across a bunch of scripts

--- inc/searchbuttons.php
+++ inc/searchbuttons.php
@@ -7,7 +7,6 @@ if($results){
 	$q = "SELECT * FROM `keeper` WHERE `isbn13`='$isbn13' && `available`='1';";
 	$tradeq = $db->query($q) or die("BAD SQL: ");
 	$trade=$tradeq->num_rows;
-	echo "trade" . $trade;
 }else{ //item foreach statement
 	$isbn13=$item['volumeInfo']['industryIdentifiers'][0]['identifier'];
 	$q = "SELECT * FROM `keeper` WHERE `isbn13`='$isbn13' && `available`='1';";
